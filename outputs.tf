b2342dddf87e66055f5d87b960e827379ad52387
Sean Sundberg <seansund@us.ibm.com>
2021-03-31 14:05:51 -0500

    Initial implementation of module
    - Generates key pair if not provided
    - Registers key with vpc infrastructure
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,17 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "id" {
+  description = "The id of the ssh key"
+  value       = ibm_is_ssh_key.key.id
+}
+
+output "public_key" {
+  description = "The public key value"
+  value       = local.public_key
+  depends_on  = [ibm_is_ssh_key.key]
+}
+
+output "private_key" {
+  description = "The private key value"
+  value       = local.public_key
+  depends_on  = [ibm_is_ssh_key.key]
+  sensitive   = true
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,39 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
 
+variable "resource_group_name" {
+  type        = string
+  description = "Name of resource group in which to create the ssh key instance. "
+}
+
+variable "region" {
+  type        = string
+  description = "Region"
+}
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud api key used to provision the IBM Cloud resources"
+}
+
+variable "name_prefix" {
+  type        = string
+  default     = ""
+  description = "(Optional) Prefix used to name all resources. If not provided then resource_group_name is used."
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "(Optional) Name given to the ssh key instance. If not provided it will be generated using prefix_name"
+}
+
+variable "public_key" {
+  type        = string
+  default     = ""
+  description = "(Optional) The public key provided for the ssh key. If not provided it will be generated"
+}
+
+variable "private_key" {
+  type        = string
+  default     = ""
+  description = "(Optional) The public key provided for the ssh key. If not provided it will be generated"
+}
