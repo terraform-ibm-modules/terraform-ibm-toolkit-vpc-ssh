2cf7d888b829df20c92356af8393fa59e50fa102
Sean Sundberg <seansund@us.ibm.com>
2021-03-31 14:10:55 -0500

    Cleans up name_prefix to match naming standards
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  name_prefix   = var.name_prefix != "" ? var.name_prefix : var.resource_group_name
+  name_prefix   = lower(replace(var.name_prefix != "" ? var.name_prefix : var.resource_group_name, "_", "-"))
   name          = var.name != "" ? var.name : "${replace(local.name_prefix, "/[^a-zA-Z0-9_\\-\\.]/", "")}-key"
   keys_provided = var.public_key != "" && var.private_key != ""
   public_key    = local.keys_provided ? var.public_key : tls_private_key.generated_key[0].public_key_openssh
