28b706ad03d2f8d7932cd4bca427b1a28e7f1237
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 12:34:39 -0500

    Adds dependency on resource-group module (#5)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-vpcssh.tf
+++ test/stages/stage2-vpcssh.tf
@@ -1,7 +1,7 @@
 module "vpc-ssh" {
   source = "./module"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   region              = var.region
   ibmcloud_api_key    = var.ibmcloud_api_key
   name_prefix         = var.name_prefix
