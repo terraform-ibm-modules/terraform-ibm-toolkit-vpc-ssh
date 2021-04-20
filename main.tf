8833e744066d9ee4962df8f429e0fe5ccabc9481
Sean Sundberg <seansund@us.ibm.com>
2021-04-20 16:38:12 -0500

    Exposes name suffix as label variable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   name_prefix   = lower(replace(var.name_prefix != "" ? var.name_prefix : var.resource_group_name, "_", "-"))
-  name          = var.name != "" ? var.name : "${replace(local.name_prefix, "/[^a-zA-Z0-9_\\-\\.]/", "")}-key"
+  name          = var.name != "" ? var.name : "${replace(local.name_prefix, "/[^a-zA-Z0-9_\\-\\.]/", "")}-${var.label}"
   keys_provided = var.public_key != "" && var.private_key != ""
   public_key    = local.keys_provided ? var.public_key : tls_private_key.generated_key[0].public_key_openssh
   private_key   = local.keys_provided ? var.private_key : tls_private_key.generated_key[0].private_key_pem
