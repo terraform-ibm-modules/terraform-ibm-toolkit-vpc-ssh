32ae66ecdbe681a15d7b8c19c06047fd9ed7cfc4
Sean Sundberg <seansund@us.ibm.com>
2021-08-04 11:39:27 -0500

    Makes private key optional
    - If only the public key is provided but not the private key, the provided public key will be used instead of generating new public and private keys
    
    Fixes #16
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 locals {
   name_prefix        = lower(replace(var.name_prefix != "" ? var.name_prefix : var.resource_group_name, "_", "-"))
   name               = var.name != "" ? var.name : "${replace(local.name_prefix, "/[^a-zA-Z0-9_\\-\\.]/", "")}-${var.label}"
-  keys_provided      = var.public_key != "" && var.private_key != ""
+  keys_provided      = var.public_key != ""
   key_files_provided = var.public_key_file != "" && var.private_key_file != ""
-  public_key         = local.key_files_provided ? file(var.public_key_file) : (local.keys_provided ? var.public_key : trimspace(tls_private_key.generated_key[0].public_key_openssh))
-  private_key        = local.key_files_provided ? file(var.private_key_file) : (local.keys_provided ? var.private_key : trimspace(tls_private_key.generated_key[0].private_key_pem))
+  public_key         = var.public_key_file != "" ? file(var.public_key_file) : (local.keys_provided ? var.public_key : trimspace(tls_private_key.generated_key[0].public_key_openssh))
+  private_key        = var.private_key_file != "" ? file(var.private_key_file) : (local.keys_provided ? var.private_key : trimspace(tls_private_key.generated_key[0].private_key_pem))
 }
 
 resource null_resource print_names {
@@ -20,8 +20,6 @@ data ibm_resource_group resource_group {
 }
 
 resource tls_private_key generated_key {
-  count = !local.keys_provided ? 1 : 0
-
   algorithm = "RSA"
   rsa_bits  = var.rsa_bits
 }
