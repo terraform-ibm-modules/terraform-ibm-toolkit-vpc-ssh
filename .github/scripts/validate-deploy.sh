c7e37b72e849665bfc6886ece36796a080264bb0
Sean Sundberg <seansund@us.ibm.com>
2021-08-04 12:13:59 -0500

    Updates test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-PROVIDED_PUBLIC_KEY=$(cat terraform.tfvars | grep public_key | sed 's/public_key="(.*)"/\1/g')
-PROVIDED_PRIVATE_KEY=$(cat terraform.tfvars | grep private_key | sed 's/private_key="(.*)"/\1/g')
+PROVIDED_PUBLIC_KEY=$(cat terraform.tfvars | grep "public_key=" | sed 's/public_key="(.*)"/\1/g')
+PROVIDED_PRIVATE_KEY=$(cat terraform.tfvars | grep "private_key=" | sed 's/private_key="(.*)"/\1/g')
 PUBLIC_KEY=$(cat .public-key)
 PRIVATE_KEY=$(cat .private-key)
+
+echo "Provided public key: ${PROVIDED_PUBLIC_KEY}"
+echo "Provided private key: ${PROVIDED_PRIVATE_KEY}"
 
 if [[ -z "${PROVIDED_PUBLIC_KEY}" ]] && [[ -z "${PRIVATE_KEY}" ]] || [[ -z "${PUBLIC_KEY}" ]]; then
   echo "No public key provided. Should generate public and private key"
